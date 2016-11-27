1e4ee31bd74ea366f5cd24c9a9db679ec8cc4bb2
Roey Chen <roeyc@vmware.com>
2016-11-27 03:53:43 -0800

    WIP: NSXv3 Client: Add paginated response
    Change-Id: I8fb23fb239c0c2d9f5d08085dabcb2e96384b8fd

--- vmware_nsxlib/v3/client.py
+++ vmware_nsxlib/v3/client.py
@@ -27,6 +27,7 @@ LOG = log.getLogger(__name__)
 ERRORS = {requests.codes.NOT_FOUND: exceptions.ResourceNotFound,
           requests.codes.PRECONDITION_FAILED: exceptions.StaleRevision}
 DEFAULT_ERROR = exceptions.ManagerError
+NULL_CURSOR_PREFIX = '0000'
 
 
 class RESTClient(object):
@@ -54,8 +55,8 @@ class RESTClient(object):
             default_headers=self._default_headers,
             client_obj=self)
 
-    def list(self, headers=None):
-        return self.url_list('')
+    def list(self, resource, headers=None):
+        return self.url_list(resource, headers=headers)
 
     def get(self, uuid, headers=None):
         return self.url_get(uuid, headers=headers)
@@ -70,7 +71,16 @@ class RESTClient(object):
         return self.url_post(resource, body, headers=headers)
 
     def url_list(self, url, headers=None):
-        return self.url_get(url, headers=headers)
+        concanated_response = self.url_get(url, headers=headers)
+        cursor = concanated_response.get('cursor', NULL_CURSOR_PREFIX)
+        op = '&' if urlparse.urlparse(url).query else '?'
+        url += op + 'cursor='
+
+        while not cursor.startswith(NULL_CURSOR_PREFIX):
+            page = self.url_get(url + cursor, headers=headers)
+            concanated_response['results'].extend(page.get('results', []))
+            cursor = page.get('cursor', NULL_CURSOR_PREFIX)
+        return concanated_response
 
     def url_get(self, url, headers=None):
         return self._rest_call(url, method='GET', headers=headers)
