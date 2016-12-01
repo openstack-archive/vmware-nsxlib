98eaec0fcc0a7a75b58e5b3597dfbe2af95ce891
Roey Chen <roeyc@vmware.com>
2016-12-01 06:25:53 -0800

    NSXv3: Add support for firewall exclude list API
    Change-Id: I49935f818b8425d9e146daf5db0d26cc057d3385

--- vmware_nsxlib/v3/security.py
+++ vmware_nsxlib/v3/security.py
@@ -204,6 +204,17 @@ class NsxLibNsGroup(utils.NsxLibApiBase):
 
 class NsxLibFirewallSection(utils.NsxLibApiBase):
 
+    def add_member_to_fw_exclude_list(self, target_id):
+        resource = 'firewall/excludelist?action=add_member'
+        body = {"target_id": target_id,
+                "target_type": consts.TARGET_TYPE_LOGICAL_PORT}
+        self.client.create(resource, body)
+
+    def remove_member_from_fw_exclude_list(self, target_id, target_type):
+        resource = ('firewall/excludelist?action=remove_member&object_id='
+                    + target_id)
+        self.client.create(resource)
+
     def _get_direction(self, sg_rule):
         return (
             consts.IN if sg_rule['direction'] == 'ingress'
