1cbc5d794249e1e496c9205e395ab03f2d44c47b
Abhishek Raut <rauta@vmware.com>
2017-01-14 15:10:09 -0800

    Add 'applied_tos' arg while creating FirewallRule
    Allow creating a firewall rule with applied_tos parameter to
    specify the target for rule.
    
    Change-Id: I0c5f1989c97b99978a57972cac05258126c4cff3

--- vmware_nsxlib/v3/security.py
+++ vmware_nsxlib/v3/security.py
@@ -373,7 +373,7 @@ class NsxLibFirewallSection(utils.NsxLibApiBase):
     def get_rule_dict(self, display_name, sources=None, destinations=None,
                       direction=consts.IN_OUT, ip_protocol=consts.IPV4_IPV6,
                       services=None, action=consts.FW_ACTION_ALLOW,
-                      logged=False, disabled=False):
+                      logged=False, disabled=False, applied_tos=None):
         rule_dict = {'display_name': display_name,
                      'direction': direction,
                      'ip_protocol': ip_protocol,
@@ -383,6 +383,8 @@ class NsxLibFirewallSection(utils.NsxLibApiBase):
                      'sources': sources or [],
                      'destinations': destinations or [],
                      'services': services or []}
+        if applied_tos is not None:
+            rule_dict['applied_tos'] = applied_tos
         return rule_dict
 
     def add_rule(self, rule, section_id):
