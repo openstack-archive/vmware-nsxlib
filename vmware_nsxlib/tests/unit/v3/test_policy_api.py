f0555890fa04c7c011b3d2cd6588b07ac2abff55
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2017-03-10 15:43:03 -0800

    [WIP] NSX Policy resources
    Define a model for NSX Policy resources API
    Supported resources:
    - domain
    - group
    - service (with a single L4 service)
    - contract (with a single entry)
    - contract map
    
    Co-Authored-by: Adit Sarfaty <asarfaty@vmware.com>
    Change-Id: I6037e2a3923d99dbfa1a3b3582e47ee853e1f146

---
+++ vmware_nsxlib/tests/unit/v3/test_policy_api.py
@@ -0,0 +1,240 @@
+# Copyright 2017 VMware, Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+from vmware_nsxlib.tests.unit.v3 import nsxlib_testcase
+from vmware_nsxlib.v3 import client
+from vmware_nsxlib.v3 import policy_constants
+from vmware_nsxlib.v3 import policy_defs as policy
+
+BASE_POLICY_URI = "https://1.2.3.4/api/v1/"
+
+
+class TestPolicyApi(nsxlib_testcase.NsxClientTestCase):
+
+    def setUp(self):
+        self.client = self.new_mocked_client(client.NSX3Client,
+                                             url_prefix='api/v1/')
+        self.policy_api = policy.NsxPolicyApi(self.client)
+
+        super(TestPolicyApi, self).setUp()
+
+    def assert_json_call(self, method, client, url, data=None):
+        url = BASE_POLICY_URI + url
+        return super(TestPolicyApi, self).assert_json_call(
+            method, client, url, data=data)
+
+
+class TestPolicyDomain(TestPolicyApi):
+
+    def test_create(self):
+        domain_def = policy.DomainDef(
+            'archaea',
+            'prokaryotic cells',
+            'typically characterized by membrane lipids')
+        self.policy_api.create(domain_def)
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/domains/archaea',
+                              data=domain_def.get_obj_dict())
+
+    def test_delete(self):
+        domain_def = policy.DomainDef('bacteria')
+        self.policy_api.delete(domain_def)
+        self.assert_json_call('DELETE', self.client,
+                              'tenants/infra/domains/bacteria')
+
+    def test_get(self):
+        domain_def = policy.DomainDef('eukarya')
+        self.policy_api.get(domain_def)
+        self.assert_json_call('GET', self.client,
+                              'tenants/infra/domains/eukarya')
+
+    def test_list(self):
+        domain_def = policy.DomainDef()
+        self.policy_api.list(domain_def)
+        self.assert_json_call('GET', self.client, 'tenants/infra/domains')
+
+
+class TestPolicyGroup(TestPolicyApi):
+
+    def test_create(self):
+        group_def = policy.GroupDef(
+            'eukarya',
+            'cats',
+            'felis catus')
+        self.policy_api.create(group_def)
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/domains/eukarya/groups/cats',
+                              data=group_def.get_obj_dict())
+
+    def test_create_with_domain(self):
+        domain_def = policy.DomainDef('eukarya',
+                                      'eukarya',
+                                      'dude with cell membranes')
+        group_def = policy.GroupDef('eukarya',
+                                    'cats',
+                                    'Ailuropoda melanoleuca')
+
+        self.policy_api.create_with_parent(domain_def, group_def)
+        data = domain_def.get_obj_dict()
+        data['groups'] = [group_def.get_obj_dict()]
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/domains/eukarya',
+                              data=data)
+
+    def test_create_with_single_tag(self):
+        domain_def = policy.DomainDef('eukarya')
+        group_def = policy.GroupDef('eukarya', 'dogs',
+                                    conditions=policy.Condition('spaniel'))
+        self.policy_api.create_with_parent(domain_def, group_def)
+        data = domain_def.get_obj_dict()
+        data['groups'] = [group_def.get_obj_dict()]
+
+        # validate body structure and defaults
+        expected_condition = {'value': 'spaniel',
+                              'operator': 'EQUALS',
+                              'member_type': 'LogicalPort',
+                              'resource_type': 'Condition',
+                              'key': 'Tag'}
+        expected_group = {'id': 'dogs',
+                          'display_name': None,
+                          'description': None,
+                          'expression': [expected_condition],
+                          '_revision': 0}
+        expected_data = {'id': 'eukarya',
+                         'display_name': None,
+                         'description': None,
+                         'groups': [expected_group],
+                         '_revision': 0}
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/domains/eukarya',
+                              data=expected_data)
+
+    def test_create_with_multi_tag(self):
+        domain_def = policy.DomainDef('eukarya')
+        pines = policy.Condition(
+            'pine',
+            operator=policy_constants.CONDITION_OP_CONTAINS)
+        maples = policy.Condition(
+            'maple',
+            operator=policy_constants.CONDITION_OP_STARTS_WITH)
+        group_def = policy.GroupDef('eukarya', 'trees',
+                                    conditions=[pines, maples])
+        self.policy_api.create_with_parent(domain_def, group_def)
+        data = domain_def.get_obj_dict()
+        data['groups'] = [group_def.get_obj_dict()]
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/domains/eukarya',
+                              data=data)
+
+    def test_delete(self):
+        group_def = policy.GroupDef(domain_id='eukarya', group_id='giraffe')
+        self.policy_api.delete(group_def)
+        self.assert_json_call('DELETE', self.client,
+                              'tenants/infra/domains/eukarya/groups/giraffe')
+
+
+class TestPolicyService(TestPolicyApi):
+
+    def test_create(self):
+        service_def = policy.ServiceDef('roomservice')
+        self.policy_api.create(service_def)
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/services/roomservice',
+                              data=service_def.get_obj_dict())
+
+    def test_create_with_parent(self):
+        service_def = policy.ServiceDef('roomservice')
+        entry_def = policy.L4ServiceEntryDef('roomservice',
+                                             'http',
+                                             name='room http',
+                                             dest_ports=[80, 8080])
+
+        self.policy_api.create_with_parent(service_def, entry_def)
+        expected_entry = {'id': 'http',
+                          'resource_type': 'L4PortSetServiceEntry',
+                          'display_name': 'room http',
+                          'description': None,
+                          'l4_protocol': 'TCP',
+                          'destination_ports': [80, 8080],
+                          '_revision': 0}
+        expected_data = {'id': 'roomservice',
+                         'display_name': None,
+                         'description': None,
+                         'service_entries': [expected_entry],
+                         '_revision': 0}
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/services/roomservice',
+                              data=expected_data)
+
+
+class TestPolicyContract(TestPolicyApi):
+
+    def test_create(self):
+        contract_def = policy.ContractDef('rental')
+        self.policy_api.create(contract_def)
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/contracts/rental',
+                              data=contract_def.get_obj_dict())
+
+    def test_create_with_parent(self):
+        contract_def = policy.ContractDef('rental')
+        entry_def = policy.ContractEntryDef('rental',
+                                            'room1',
+                                            description='includes roomservice',
+                                            services=["roomservice"])
+
+        self.policy_api.create_with_parent(contract_def, entry_def)
+        expected_entry = {'id': 'room1',
+                          'display_name': None,
+                          'description': 'includes roomservice',
+                          'services': ["roomservice"],
+                          'action': 'ALLOW',
+                          '_revision': 0}
+        expected_data = {'id': 'rental',
+                         'display_name': None,
+                         'description': None,
+                         'contract_entries': [expected_entry],
+                         '_revision': 0}
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/contracts/rental',
+                              data=expected_data)
+
+
+class TestPolicyContractMap(TestPolicyApi):
+
+    def test_create(self):
+        contract_map_def = policy.ContractMapDef('d1', 'cm1',
+                                                 sequence_number=12,
+                                                 source_groups=["group1",
+                                                                "group2"],
+                                                 dest_groups=["group1"],
+                                                 contract_id="contract1")
+        self.policy_api.create(contract_map_def)
+        expected_data = {'_revision': 0,
+                         'id': 'cm1',
+                         'display_name': None,
+                         'description': None,
+                         'sequence_number': 12,
+                         'source_groups':
+                         ['/tenants/infra/domains/d1/groups/group1',
+                          '/tenants/infra/domains/d1/groups/group2'],
+                         'destination_groups':
+                         ['/tenants/infra/domains/d1/groups/group1'],
+                         'contract_path': '/tenants/infra/contracts/contract1'}
+
+        self.assert_json_call('PUT', self.client,
+                              'tenants/infra/domains/d1/connectivity-rules/'
+                              'contract-maps/cm1',
+                              data=expected_data)
---
+++ vmware_nsxlib/v3/policy_resources.py
@@ -0,0 +1,340 @@
+# Copyright 2017 VMware, Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import abc
+import six
+import uuid
+
+from vmware_nsxlib.v3 import exceptions
+from vmware_nsxlib.v3 import policy_constants
+from vmware_nsxlib.v3 import policy_defs
+
+# TODO(asarfaty): support retries?
+# TODO(asarfaty): support different tenants
+# TODO(asarfaty): use PATCH for update when the backend supports it
+
+
+@six.add_metaclass(abc.ABCMeta)
+class NsxPolicyResourceBase(object):
+    """Abstract class for NSX policy resources
+
+    declaring the basic apis each policy resource should support
+    """
+    def __init__(self, policy_api):
+        self.policy_api = policy_api
+
+    @abc.abstractmethod
+    def list(self):
+        pass
+
+    @abc.abstractmethod
+    def get(self, uuid):
+        pass
+
+    @abc.abstractmethod
+    def delete(self, uuid):
+        pass
+
+    @abc.abstractmethod
+    def create(self, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def update(self, uuid, *args, **kwargs):
+        pass
+
+    @staticmethod
+    def _init_obj_uuid(obj_uuid):
+        if not obj_uuid:
+            # generate a random id
+            obj_uuid = str(uuid.uuid4())
+        return obj_uuid
+
+    def get_by_name(self, name):
+        # Return first match by name
+        resources_list = self.list()
+        for obj in resources_list:
+            if obj.get('display_name') == name:
+                return obj
+
+
+class NsxPolicyDomainApi(NsxPolicyResourceBase):
+    """NSX Policy Domain."""
+    def create(self, name, domain_id=None, description=None):
+        domain_id = self._init_obj_uuid(domain_id)
+        domain_def = policy_defs.DomainDef(domain_id=domain_id,
+                                           name=name,
+                                           description=description)
+        return self.policy_api.create(domain_def)
+
+    def delete(self, domain_id):
+        domain_def = policy_defs.DomainDef(domain_id)
+        self.policy_api.delete(domain_def)
+
+    def get(self, domain_id):
+        domain_def = policy_defs.DomainDef(domain_id)
+        return self.policy_api.get(domain_def)
+
+    def list(self):
+        domain_def = policy_defs.DomainDef()
+        return self.policy_api.list(domain_def)['results']
+
+    def update(self, domain_id, name=None, description=None):
+        domain_def = policy_defs.DomainDef(domain_id=domain_id)
+        # Get the current data, and update it with the new values
+        domain = self.get(domain_id)
+        domain_def.update_attributes_in_body(domain,
+                                             name=name,
+                                             description=description)
+        # update the backend
+        return self.policy_api.update(domain_def, domain)
+
+
+class NsxPolicyGroupApi(NsxPolicyResourceBase):
+    """NSX Policy Group (under a Domain)."""
+    def create(self, name, domain_id, group_id=None,
+               description=None, conditions=None):
+        """Create a group under a specific domain."""
+        # TODO(asarfaty) add condition parameters here
+        group_id = self._init_obj_uuid(group_id)
+        group_def = policy_defs.GroupDef(domain_id=domain_id,
+                                         group_id=group_id,
+                                         name=name,
+                                         description=description,
+                                         conditions=conditions)
+        return self.policy_api.create(group_def)
+
+    def delete(self, domain_id, group_id):
+        group_def = policy_defs.GroupDef(domain_id=domain_id,
+                                         group_id=group_id)
+        self.policy_api.delete(group_def)
+
+    def get(self, domain_id, group_id):
+        group_def = policy_defs.GroupDef(domain_id=domain_id,
+                                         group_id=group_id)
+        return self.policy_api.get(group_def)
+
+    def list(self, domain_id):
+        """List all the groups of a specific domain."""
+        group_def = policy_defs.GroupDef(domain_id=domain_id)
+        return self.policy_api.list(group_def)['results']
+
+    def get_by_name(self, domain_id, name):
+        """Return first group matched by name of this domain"""
+        resources_list = self.list(domain_id)
+        for obj in resources_list:
+            if obj.get('display_name') == name:
+                return obj
+
+    def update(self, domain_id, group_id,
+               name=None, description=None, conditions=None):
+        # TODO(asarfaty) add condition parameters here
+        group_def = policy_defs.GroupDef(domain_id=domain_id,
+                                         group_id=group_id)
+        # Get the current data, and update it with the new values
+        group = self.get(domain_id, group_id)
+        group_def.update_attributes_in_body(group,
+                                            name=name,
+                                            description=description,
+                                            conditions=conditions)
+        # update the backend
+        return self.policy_api.update(group_def, group)
+
+
+class NsxPolicyL4ServiceApi(NsxPolicyResourceBase):
+    """NSX Policy Service (with a single L4 service entry).
+
+    Note the nsx-policy backend supports different types of service entries,
+    and multiple service entries per service.
+    At this point this is not supported here.
+    """
+    def create(self, name, service_id=None, description=None,
+               protocol=policy_constants.TCP, dest_ports=None):
+        service_id = self._init_obj_uuid(service_id)
+        service_def = policy_defs.ServiceDef(service_id=service_id,
+                                             name=name,
+                                             description=description)
+        # NOTE(asarfaty) We set the service entry display name (which is also
+        # used as the id) to be the same as the service name. In case we
+        # support multiple service entries, we need the name to be unique.
+        entry_def = policy_defs.L4ServiceEntryDef(
+            service_id=service_id,
+            name=name,
+            description=description,
+            protocol=protocol,
+            dest_ports=dest_ports)
+
+        return self.policy_api.create_with_parent(service_def, entry_def)
+
+    def create_without_entry(self, name, service_id=None, description=None):
+        # TODO(asarfaty) not sure this is actually needed.
+        service_id = self._init_obj_uuid(service_id)
+        service_def = policy_defs.ServiceDef(service_id=service_id,
+                                             name=name,
+                                             description=description)
+        return self.policy_api.create(service_def)
+
+    def delete(self, service_id):
+        service_def = policy_defs.ServiceDef(service_id=service_id)
+        self.policy_api.delete(service_def)
+
+    def get(self, service_id):
+        service_def = policy_defs.ServiceDef(service_id=service_id)
+        return self.policy_api.get(service_def)
+
+    def list(self):
+        service_def = policy_defs.ServiceDef()
+        return self.policy_api.list(service_def)['results']
+
+    def update(self, service_id, name=None, description=None,
+               protocol=None, dest_ports=None):
+        service_def = policy_defs.ServiceDef(service_id=service_id)
+        # Get the current data, and update it with the new values
+        service = self.get(service_id)
+        service_def.update_attributes_in_body(service,
+                                              name=name,
+                                              description=description)
+        # TODO(asarfaty): Update the service entry too (protocol, dest_ports)
+
+        # update the backend
+        return self.policy_api.update(service_def, service)
+
+
+class NsxContractApi(NsxPolicyResourceBase):
+    """NSX Policy Contract (with a single contract-entry).
+
+    Note the nsx-policy backend supports multiple entries per contract.
+    At this point this is not supported here.
+
+    Services should be a list of service ids
+    """
+    def create(self, name, contract_id=None, description=None,
+               services=None, action=policy_constants.CONTRACT_ALLOW):
+        contract_id = self._init_obj_uuid(contract_id)
+        contract_def = policy_defs.ContractDef(contract_id=contract_id,
+                                               name=name,
+                                               description=description)
+        # NOTE(asarfaty) We set the contract entry display name (which is also
+        # used as the id) to be the same as the contract name. In case we
+        # support multiple contract entries, we need the name to be unique.
+        entry_def = policy_defs.ContractEntryDef(contract_id=contract_id,
+                                                 name=name,
+                                                 description=description,
+                                                 services=services,
+                                                 action=action)
+
+        return self.policy_api.create_with_parent(contract_def, entry_def)
+
+    def delete(self, contract_id):
+        contract_def = policy_defs.ContractDef(contract_id=contract_id)
+        self.policy_api.delete(contract_def)
+
+    def get(self, contract_id):
+        contract_def = policy_defs.ContractDef(contract_id=contract_id)
+        return self.policy_api.get(contract_def)
+
+    def list(self):
+        contract_def = policy_defs.ContractDef()
+        return self.policy_api.list(contract_def)['results']
+
+    def update(self, contract_id, name=None, description=None):
+        contract_def = policy_defs.ContractDef(contract_id=contract_id)
+        # Get the current data, and update it with the new values
+        contract = self.get(contract_id)
+        contract_def.update_attributes_in_body(contract,
+                                               name=name,
+                                               description=description)
+
+        # TODO(asarfaty): Update the contract entry too (services / action)
+
+        # update the backend
+        return self.policy_api.update(contract_def, contract)
+
+
+class NsxContractMapApi(NsxPolicyResourceBase):
+    """NSX Policy ContractMap (Under a Domain)."""
+    def _get_next_seq_num(self, domain_id):
+        # get the current entries, and choose the next unused sequence number
+        contract_maps = self.list(domain_id)
+        if not contract_maps:
+            return 0
+
+        seq_nums = [int(cm['sequence_number']) for cm in contract_maps]
+        seq_nums.sort()
+        return seq_nums[-1] + 1
+
+    def create(self, name, domain_id, contractmap_id=None,
+               description=None, sequence_number=None, contract_id=None,
+               source_groups=None, dest_groups=None):
+        # Validate and convert inputs
+        contractmap_id = self._init_obj_uuid(contractmap_id)
+        if not contract_id:
+            # contract_id must be provided
+            err_msg = (_("Cannot create a contract map %(name)s without "
+                         "contract id") % {'name': name})
+            raise exceptions.ManagerError(details=err_msg)
+        if sequence_number is None:
+            # get the next available sequence number
+            sequence_number = self._get_next_seq_num(domain_id)
+
+        contractmap_def = policy_defs.ContractMapDef(
+            domain_id=domain_id,
+            contractmap_id=contractmap_id,
+            name=name,
+            description=description,
+            sequence_number=sequence_number,
+            source_groups=source_groups,
+            dest_groups=dest_groups,
+            contract_id=contract_id)
+        return self.policy_api.create(contractmap_def)
+
+    def delete(self, domain_id, contractmap_id):
+        contractmap_def = policy_defs.ContractMapDef(
+            domain_id=domain_id,
+            contractmap_id=contractmap_id)
+        self.policy_api.delete(contractmap_def)
+
+    def get(self, domain_id, contractmap_id):
+        contractmap_def = policy_defs.ContractMapDef(
+            domain_id=domain_id,
+            contractmap_id=contractmap_id)
+        return self.policy_api.get(contractmap_def)
+
+    def list(self, domain_id):
+        """List all the groups of a specific domain."""
+        contractmap_def = policy_defs.ContractMapDef(
+            domain_id=domain_id)
+        return self.policy_api.list(contractmap_def)['results']
+
+    def update(self, domain_id, contractmap_id, name=None,
+               description=None, sequence_number=None, contract_id=None,
+               source_groups=None, dest_groups=None):
+        contractmap_def = policy_defs.ContractMapDef(
+            domain_id=domain_id,
+            contractmap_id=contractmap_id)
+        # Get the current data, and update it with the new values
+        contractmap = self.get(domain_id, contractmap_id)
+        contractmap_def.update_attributes_in_body(
+            contractmap,
+            name=name,
+            description=description,
+            sequence_number=sequence_number,
+            contract_id=contract_id,
+            source_groups=source_groups,
+            dest_groups=dest_groups)
+
+        # update the backend
+        return self.policy_api.update(contractmap_def, contractmap)
