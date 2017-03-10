f65d44d9a52c6f3e8d1025f8fb23a25be4bc1ba1
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2017-03-10 15:43:03 -0800

    [WIP] NSX Policy resources
    Define a model for NSX Policy resources API
    Supported resources:
    - domain
    - group
    - service (with a single L4 service)
    - contract (with a single entry)
    - contract map
    
    Co-Authored-by: Adit Sarfaty <asarfaty@vmware.com>
    Change-Id: I6037e2a3923d99dbfa1a3b3582e47ee853e1f146

---
+++ vmware_nsxlib/v3/policy_constants.py
@@ -0,0 +1,34 @@
+# Copyright 2017 VMware, Inc.
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+TCP = 'TCP'
+UDP = 'UDP'
+
+POLICY_INFRA_TENANT = 'infra'
+
+CONTRACT_ALLOW = 'ALLOW'
+CONTRACT_DENY = 'DENY'
+
+ANY_GROUP = "ANY"
+
+CONDITION_KEY_TAG = 'Tag'
+CONDITION_MEMBER_VM = 'VirtualMachine'
+CONDITION_MEMBER_PORT = 'LogicalPort'
+CONDITION_MEMBER_NET = 'LogicalSwitch'
+CONDITION_OP_EQUALS = 'EQUALS'
+CONDITION_OP_CONTAINS = 'CONTAINS'
+CONDITION_OP_STARTS_WITH = 'STARTSWITH'
+
+DEFAULT_THUMBPRINT = 'abc'
