6712696f6d2a27ac97398f581dc8fe1962111f68
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2017-07-25 17:49:26 -0700

    Nsx policy: adjust to latest backend changes
    1. dashes between words convention in uris
    2. rename DENY action to DROP
    3. Remove LogicalSwitch membership criteria
    
    Change-Id: Ida3028a89f17aa11979eb800e7e3a5b14f344edd

--- vmware_nsxlib/v3/policy_defs.py
+++ vmware_nsxlib/v3/policy_defs.py
@@ -470,7 +470,7 @@ class EnforcementPointDef(ResourceDef):
     @property
     def path_pattern(self):
         return (TENANTS_PATH_PATTERN +
-                'deploymentzones/default/enforcementpoints/')
+                'deployment-zones/default/enforcement-points/')
 
     def get_obj_dict(self):
         body = super(EnforcementPointDef, self).get_obj_dict()
@@ -526,7 +526,7 @@ class DeploymentMapDef(ResourceDef):
 
     @property
     def path_pattern(self):
-        return (DOMAINS_PATH_PATTERN + '%s/domaindeploymentmaps/')
+        return (DOMAINS_PATH_PATTERN + '%s/domain-deployment-maps/')
 
     def get_obj_dict(self):
         body = super(DeploymentMapDef, self).get_obj_dict()
