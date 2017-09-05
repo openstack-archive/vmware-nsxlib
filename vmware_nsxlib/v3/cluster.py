72ec4efc2bf872cafc60b070870f6b6a98182007
Adit Sarfaty <asarfaty@vmware.com>
2017-09-05 15:55:28 +0300

    Fix request with retry code
    Calling request_with_retry_on_ssl_error should be done
    with 'self' object
    
    Change-Id: Icec9bb7b8c1c667cd3ac5a52636984379be318be

--- vmware_nsxlib/v3/cluster.py
+++ vmware_nsxlib/v3/cluster.py
@@ -132,7 +132,7 @@ class TimeoutSession(requests.Session):
 
         if self.cert is not None:
             # Recursive call - shouldn't happen
-            return request_with_retry_on_ssl_error(*args, **kwargs)
+            return request_with_retry_on_ssl_error(self, *args, **kwargs)
 
         # The following with statement allows for preparing certificate and
         # private key file and dispose it at the end of request
@@ -147,7 +147,7 @@ class TimeoutSession(requests.Session):
         with get_cert_provider() as provider:
             self.cert = provider.filename()
             try:
-                ret = request_with_retry_on_ssl_error(*args, **kwargs)
+                ret = request_with_retry_on_ssl_error(self, *args, **kwargs)
             except Exception as e:
                 self.cert = None
                 raise e
