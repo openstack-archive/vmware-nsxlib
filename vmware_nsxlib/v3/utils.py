216d8a3be98e17a664afd6a40a98667c3ccc1f0c
Gary Kotton <gkotton@vmware.com>
2017-10-24 23:41:56 -0700

    Provide a callback to inject headers to the NSX
    Enable application to bind to a callback that will inject
    headers to the requests sent to the NSX. This can for example
    be: 'X-NSX-EUSER' which will provide the NSX context of the user
    on behalf of whom the opertaion is done.
    
    A new method set_inject_headers_callback(in is added. This method
    receives a callback that should return a dictionary of headers to
    be added.
    
    Change-Id: I90fa7ea8c7828bf97aec66321e4169588108760e

--- vmware_nsxlib/v3/utils.py
+++ vmware_nsxlib/v3/utils.py
@@ -32,6 +32,12 @@ MAX_RESOURCE_TYPE_LEN = 20
 MAX_TAG_LEN = 40
 DEFAULT_MAX_ATTEMPTS = 10
 DEFAULT_CACHE_AGE_SEC = 600
+INJECT_HEADERS_CALLBACK = None
+
+
+def set_inject_headers_callback(callback):
+    global INJECT_HEADERS_CALLBACK
+    INJECT_HEADERS_CALLBACK = callback
 
 
 def _validate_resource_type_length(resource_type):
