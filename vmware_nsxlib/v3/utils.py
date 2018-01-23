f8ffbe5ff9da6883c77218da5babdeedf1fadfd4
Gary Kotton <gkotton@vmware.com>
2018-01-23 02:16:06 -0800

    Ensure that max_attempts is set
    The NsxLibApiBase can have the nsxlib_config passed as None.
    In order to use the retry logic we retrieve the max_attempts from
    the client object. This is always set.
    
    Change-Id: I4ac60f9a3d89b60d9b35390825578cb7f849dfa8

--- vmware_nsxlib/v3/utils.py
+++ vmware_nsxlib/v3/utils.py
@@ -24,6 +24,7 @@ import tenacity
 from tenacity import _utils as tenacity_utils
 
 from vmware_nsxlib._i18n import _
+from vmware_nsxlib.v3 import config
 from vmware_nsxlib.v3 import exceptions as nsxlib_exceptions
 
 LOG = log.getLogger(__name__)
@@ -386,7 +387,7 @@ class NsxLibApiBase(object):
             # it again with the new revision_id
             @retry_upon_exception(
                 nsxlib_exceptions.StaleRevision,
-                max_attempts=self.nsxlib_config.max_attempts)
+                max_attempts=self.client.max_attempts)
             def do_update():
                 return self._internal_update_resource(
                     resource, payload,
@@ -410,7 +411,7 @@ class NsxLibApiBase(object):
         # Using internal method so we can access max_attempts in the decorator
         @retry_upon_exception(
             nsxlib_exceptions.StaleRevision,
-            max_attempts=self.nsxlib_config.max_attempts)
+            max_attempts=self.client.max_attempts)
         def _do_delete():
             self.client.delete(self.get_path(resource))
 
@@ -420,7 +421,7 @@ class NsxLibApiBase(object):
         # Using internal method so we can access max_attempts in the decorator
         @retry_upon_exception(
             nsxlib_exceptions.StaleRevision,
-            max_attempts=self.nsxlib_config.max_attempts)
+            max_attempts=self.client.max_attempts)
         def _do_create():
             return self.client.create(resource, body, headers=headers)
 
