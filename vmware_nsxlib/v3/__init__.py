5d98a82265e4ddc5b9e645aab75169c2810ed890
Michal Kelner Mishali <mkelnermishal@vmware.com>
2018-08-02 14:10:28 +0300

    NSX|V3: Support new icmp codes and types list
    Support changes in backend for ICMP types and codes
    while maintaining backwards compatibility.
    
    Change-Id: I7478904b5549345d7e2227ee89836e0b9dbe9d11
    Signed-off-by: Michal Kelner Mishali <mkelnermishal@vmware.com>

--- vmware_nsxlib/v3/__init__.py
+++ vmware_nsxlib/v3/__init__.py
@@ -308,6 +308,14 @@ class NsxLib(NsxLibBase):
 
     def feature_supported(self, feature):
         if (version.LooseVersion(self.get_version()) >=
+            version.LooseVersion(nsx_constants.NSX_VERSION_2_4_0)):
+            # Features available since 2.4
+            if (feature == nsx_constants.FEATURE_ENS_WITH_SEC):
+                return True
+            if (feature == nsx_constants.FEATURE_ICMP_STRICT):
+                return True
+
+        if (version.LooseVersion(self.get_version()) >=
             version.LooseVersion(nsx_constants.NSX_VERSION_2_2_0)):
             # Features available since 2.2
             if (feature == nsx_constants.FEATURE_VLAN_ROUTER_INTERFACE or
