129c6a76986a02d115584794ba22e3fabf17d81b
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2018-09-13 14:19:09 -0700

    WIP: Support Segment and NetworkSegment
    NetworkSegments belog to specific network and can't be moved.
    Segments do not belong to particluar network.
    
    Change-Id: Ifcaf9fe1776a7e68d78cef81bd77e40dfcd2aad3

--- vmware_nsxlib/v3/__init__.py
+++ vmware_nsxlib/v3/__init__.py
@@ -384,6 +384,7 @@ class NsxPolicyLib(NsxLibBase):
         self.icmp_service = policy_resources.NsxPolicyIcmpServiceApi(
             self.policy_api)
         self.network = policy_resources.NsxPolicyNetworkApi(self.policy_api)
+        self.network_segment = policy_resources.NsxPolicyNetworkSegmentApi(self.policy_api)
         self.segment = policy_resources.NsxPolicySegmentApi(self.policy_api)
         self.comm_map = policy_resources.NsxPolicyCommunicationMapApi(
             self.policy_api)
