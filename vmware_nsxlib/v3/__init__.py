2e35ccc588bfbae99d2a4e9e3455725005a8bc10
Gordon Zhang <gordonz@vmware.com>
2018-10-04 16:48:14 -0700

    Add loadbalancer policy API into vmware-nsxlib
    Change-Id: Idfdef157ba2829bc24761f7b08b07f013c9eccff

--- vmware_nsxlib/v3/__init__.py
+++ vmware_nsxlib/v3/__init__.py
@@ -28,6 +28,7 @@ from vmware_nsxlib.v3 import load_balancer
 from vmware_nsxlib.v3 import native_dhcp
 from vmware_nsxlib.v3 import nsx_constants
 from vmware_nsxlib.v3 import policy_defs
+from vmware_nsxlib.v3 import policy_load_balancer
 from vmware_nsxlib.v3 import policy_resources
 from vmware_nsxlib.v3 import resources
 from vmware_nsxlib.v3 import router
@@ -420,6 +421,8 @@ class NsxPolicyLib(NsxLibBase):
             policy_resources.NsxIpDiscoveryProfileApi(self.policy_api))
         self.mac_discovery_profile = (
             policy_resources.NsxMacDiscoveryProfileApi(self.policy_api))
+        self.load_balancer = policy_load_balancer.NsxPolicyLoadBalancerApi(
+            self.policy_api)
 
     @property
     def keepalive_section(self):
