6f48626602392bb7e8d9495192f6ef3259140058
Adit Sarfaty <asarfaty@vmware.com>
2018-11-29 10:56:59 +0200

    Add policy segemnt profiles api
    Basic functionality for segment profiles.
    The create/update actions do not include all the parameters yet since
    those reasources are mainly for read-only at the moment
    
    Change-Id: I6a4fd4df680bd3efef39060a92b88bef11ce2a20

--- vmware_nsxlib/v3/__init__.py
+++ vmware_nsxlib/v3/__init__.py
@@ -407,6 +407,16 @@ class NsxPolicyLib(NsxLibBase):
         self.deployment_map = policy_resources.NsxPolicyDeploymentMapApi(
             self.policy_api)
         self.ip_block = policy_resources.NsxPolicyIpBlockApi(self.policy_api)
+        self.segment_security_profile = (
+            policy_resources.NsxSegmentSecurityProfileApi(self.policy_api))
+        self.qos_profile = (
+            policy_resources.NsxQosProfileApi(self.policy_api))
+        self.spoofguard_profile = (
+            policy_resources.NsxSpoofguardProfileApi(self.policy_api))
+        self.ip_discovery_profile = (
+            policy_resources.NsxIpDiscoveryProfileApi(self.policy_api))
+        self.mac_discovery_profile = (
+            policy_resources.NsxMacDiscoveryProfileApi(self.policy_api))
 
     @property
     def keepalive_section(self):
