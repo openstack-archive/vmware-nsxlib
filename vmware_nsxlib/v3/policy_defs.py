b993f470edd36ad91303b8284632761436bd3860
Adit Sarfaty <asarfaty@vmware.com>
2018-12-10 09:38:44 +0200

    Improve policy realization state apis
    1. Support results with multiple entities and select the right one
    2. Add apo for waiting until the resource is realized
    
    Change-Id: I71c56da9672f493f1197bb848b54c55f286c1369

--- vmware_nsxlib/v3/policy_defs.py
+++ vmware_nsxlib/v3/policy_defs.py
@@ -79,6 +79,10 @@ class ResourceDef(object):
     def resource_type():
         pass
 
+    @staticmethod
+    def realization_entity_type():
+        pass
+
     def path_defs(self):
         pass
 
@@ -250,6 +254,10 @@ class RouteAdvertisement(object):
 class RouterDef(ResourceDef):
     def path_defs(self):
         return (TenantDef,)
+
+    @staticmethod
+    def realization_entity_type():
+        return 'RealizedLogicalRouter'
 
     def get_obj_dict(self):
         body = super(RouterDef, self).get_obj_dict()
@@ -439,6 +447,10 @@ class BaseSegmentDef(ResourceDef):
     def resource_type():
         return 'Segment'
 
+    @staticmethod
+    def realization_entity_type():
+        return 'RealizedLogicalSwitch'
+
 
 class Tier1SegmentDef(BaseSegmentDef):
     '''Tier1 segments can not move to different tier1 '''
@@ -526,6 +538,10 @@ class SegmentPortDef(ResourceDef):
     @staticmethod
     def resource_type():
         return 'SegmentPort'
+
+    @staticmethod
+    def realization_entity_type():
+        return 'RealizedLogicalPort'
 
     def path_defs(self):
         return (TenantDef, SegmentDef)
@@ -730,6 +746,10 @@ class GroupDef(ResourceDef):
     def resource_type():
         return 'Group'
 
+    @staticmethod
+    def realization_entity_type():
+        return 'RealizedGroup'
+
     def path_defs(self):
         return (TenantDef, DomainDef)
 
@@ -849,6 +869,10 @@ class CommunicationMapDef(ResourceDef):
     @staticmethod
     def resource_type():
         return 'SecurityPolicy'
+
+    @staticmethod
+    def realization_entity_type():
+        return 'RealizedFirewallSection'
 
     def path_defs(self):
         return (TenantDef, DomainDef)
