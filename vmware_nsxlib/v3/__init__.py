5b03ccc79625262ebe170891ed3be0ca5c8409bf
Adit Sarfaty <asarfaty@vmware.com>
2018-12-24 15:23:28 +0200

    Add manager status validation to validate connection
    When validating if a connection with the NSX manager is up, also check
    the manager status directly
    
    Change-Id: I5de7054a058a74e8237a3344e0951ce37976c135

--- vmware_nsxlib/v3/__init__.py
+++ vmware_nsxlib/v3/__init__.py
@@ -69,8 +69,10 @@ class NsxLibBase(object):
     def set_config(self, nsxlib_config):
         """Set config user provided and extend it according to application"""
         self.nsxlib_config = nsxlib_config
-        self.nsxlib_config.extend(keepalive_section=self.keepalive_section,
-                                  url_base=self.client_url_prefix)
+        self.nsxlib_config.extend(
+            keepalive_section=self.keepalive_section,
+            validate_connection_method=self.validate_connection_method,
+            url_base=self.client_url_prefix)
 
     @abc.abstractproperty
     def client_url_prefix(self):
@@ -78,6 +80,10 @@ class NsxLibBase(object):
 
     @abc.abstractproperty
     def keepalive_section(self):
+        pass
+
+    @abc.abstractproperty
+    def validate_connection_method(self):
         pass
 
     @abc.abstractmethod
@@ -312,6 +318,20 @@ class NsxLib(NsxLibBase):
     def keepalive_section(self):
         return 'transport-zones'
 
+    @property
+    def validate_connection_method(self):
+        """Return a method that will validate the NSX manager status"""
+        def check_manager_status(client, manager_url):
+            status = client.get('node/services/manager/status', silent=True)
+            if ('runtime_state' not in status or
+                status['runtime_state'] != 'running'):
+                msg = _("Manager is not in running state: %s") % status
+                LOG.warning(msg)
+                raise exceptions.ResourceNotFound(
+                    manager=manager_url, operation=msg)
+
+        return check_manager_status
+
     def get_version(self):
         if self.nsx_version:
             return self.nsx_version
@@ -449,6 +469,11 @@ class NsxPolicyLib(NsxLibBase):
     @property
     def keepalive_section(self):
         return 'infra'
+
+    @property
+    def validate_connection_method(self):
+        # TODO(asarfaty): Find an equivalent api to change policy status
+        pass
 
     def get_version(self):
         """Get the NSX Policy manager version
